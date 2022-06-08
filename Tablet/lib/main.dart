26ec6c348397f78c46b2064bc2f803000f7876f4
GunwooYun <gunwoo87@gmail.com>
2022-06-08 23:44:13 +0900

    timer widget home

--- Tablet/lib/main.dart
+++ Tablet/lib/main.dart
@@ -1,40 +1,56 @@
 import 'package:flutter/material.dart';
 
+import 'package:timer_builder/timer_builder.dart';
+import 'package:date_format/date_format.dart';
+
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Home(),
     );
   }
 }
 
-class Home extends StatelessWidget{
+class Home extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Reminder Door'),
-      ),
-      backgroundColor: Colors.grey,
-      body: Column(
-        children: [
-          Container(
-            height: 200,
-            color: Colors.green,
-          ),
-          Container(
-            height: 100,
-            color: Colors.red,
-          ),
-          Container(
-            height: 350,
-            color: Colors.blue,
-          ),
-        ],
-      )
-    );
+        appBar: AppBar(
+          title: Text('Reminder Door'),
+        ),
+        backgroundColor: Colors.grey,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 200,
+              //color: Colors.green,
+              //padding: const EdgeInsets.all(6.0),
+              child: TimerBuilder.periodic(
+                const Duration(seconds: 1),
+                builder: (context) {
+                  return Text(
+                    formatDate(DateTime.now(), [hh, ':', nn, ':', ss, ' ', am]),
+                    style: const TextStyle(
+                      fontSize: 180,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  );
+                },
+              ),
+            ),
+            Container(
+              height: 100,
+              color: Colors.red,
+            ),
+            Container(
+              height: 350,
+              color: Colors.blue,
+            ),
+          ],
+        ));
   }
 }
