e5bdb910c450951afe93ed6628f5c645f3d43480
GunwooYun <gunwoo87@gmail.com>
2022-06-15 23:21:29 +0900

    클래스 분리 및 날씨 레이아웃 설정

---
+++ Tablet/lib/weather.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+const apikey = '78875fa7b0b0cad43ec365684ab3eb1b';
+
+class Weather extends StatefulWidget {
+  const Weather({Key? key}) : super(key: key);
+
+  @override
+  State<Weather> createState() => _WeatherState();
+}
+
+class _WeatherState extends State<Weather> {
+
+  double latitude2 = 0;
+  double longitude2 = 0;
+
+  String cityName = "";
+  double temp = 0.0;
+
+  @override
+  void initState(){
+    super.initState();
+    getLocation();
+    fetchData();
+  }
+
+  /* 위치 조회 */
+  void getLocation() async{
+    // LocationPermission permission;
+    //permission = await Geolocator.requestPermission();
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high);
+      latitude2 = position.latitude;
+      longitude2 = position.longitude;
+      //print(position);
+      print(latitude2);
+      print(longitude2);
+    }catch(e) {
+      print('Error : Internet connection problem');
+    }
+  }
+
+  /* JSON 데이터 수신 및 파싱 */
+  void fetchData() async {
+    String urlStr = 'https://api.openweathermap.org/data/2.5/weather?lat=$latitude2&lon=$longitude2'
+        '&exclude=current&appid=$apikey';
+    http.Response response = await http.get(
+        Uri.parse(urlStr));
+
+    if (response.statusCode == 200) { /* Connected successfully */
+      print(response.body);
+      String jsonData = response.body;
+      //var myJson = jsonDecode(jsonData)['weather'][0]['description'];
+
+      temp = jsonDecode(jsonData)['main']['temp']; // 어떤 타입 변수가 리턴될 지 몰라 var타입 선언
+      cityName = jsonDecode(jsonData)['name'];
+      print(temp);
+      print(cityName);
+
+    }
+    else{
+      print(response.statusCode);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'cityName',
+              style: TextStyle(
+                fontSize: 40.0
+              ),
+            ),
+            SizedBox(
+              width: 20.0,
+            ),
+            Text(
+              'temp',
+              style: TextStyle(
+                fontSize: 40.0
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
