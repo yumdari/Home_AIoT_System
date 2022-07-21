60f9c535a496782297df1c8adbd31c65f2af9968
GunwooYun <gunwoo87@gmail.com>
2022-07-21 19:54:36 +0900

    feat : mjpeg 스트림 출력, 페이지 3개 생성

---
+++ Mobile/lib/CameraViewer.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_mjpeg/flutter_mjpeg.dart'; // Fullter widget shows mjpeg stream from URL
+
+class CameraViewer extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    final isRunning = useState(true);
+    return  Scaffold(
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Center(
+              child: Mjpeg(
+                isLive: isRunning.value,
+                error: (context, error, stack) {
+                  print(error);
+                  print(stack);
+                  return Text(error.toString(), style: TextStyle(color: Colors.red));
+                },
+                stream: 'http://10.10.141.250:8080/?action=stream',
+              ),
+            ),
+          ),
+          Row(
+            children: <Widget>[
+              RaisedButton(
+                onPressed: () {
+                  isRunning.value = !isRunning.value;
+                },
+                child: Text('Toggle'),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ Mobile/lib/homeIoT.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class homeIoT extends StatefulWidget {
+  const homeIoT({Key? key}) : super(key: key);
+
+  @override
+  State<homeIoT> createState() => _homeIoTState();
+}
+
+class _homeIoTState extends State<homeIoT> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('homeIoT page'),
+    );
+  }
+}
